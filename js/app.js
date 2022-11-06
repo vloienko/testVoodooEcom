285c6fd01fd93e523235e754967b54e8b1c8695c
vloienko <loenko99@gmail.com>
2022-11-06 15:05:21 +0200

    Custom Select - done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
+let selectWrapper = document.querySelectorAll('.select-wrapper');
+const selectTextFirst = 'Practice / Institution*';
+const selectTextSecond = 'Medical Profession*';
+console.log(selectWrapper);
+
+selectWrapper.forEach((element, index) => {
+   element.querySelector('select').remove()
+   element.insertAdjacentHTML('afterbegin', `
+      <div class="select contact__form-input">
+         <div class="select__header">
+            <span class="select__current"></span>
+            <div class="select__icon">
+               <img src="img/arrow.svg" alt="">
+            </div>
+         </div>
+         <div class="select__body">
+            <div class="select__item">Option 1</div>
+            <div class="select__item">Option 2</div>
+            <div class="select__item">Option 3</div>
+            <div class="select__item">Option 4</div>
+            <div class="select__item">Option 5</div>
+         </div>
+      </div>`
+   );
+
+   if (index === 0) {
+      element.querySelector('.select__current').innerHTML = selectTextFirst;
+   }
+   if (index === 1) {
+      element.querySelector('.select__current').innerHTML = selectTextSecond;
+   }
+});
+
+
 let select = function () {
    let selectHeader = document.querySelectorAll('.select__header');
    let selectItem = document.querySelectorAll('.select__item');
